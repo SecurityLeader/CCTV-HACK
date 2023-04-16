0d4daa965660c5cccc519383b3dd9dc24dded4da
Mr.Robot <128728937+SecurityLeader@users.noreply.github.com>
2023-04-16 18:16:13 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt install python -y
+apt install figlet
+apt install python2 -y
+pip install requests
+toilet -f mono12 -F metal Mr.Robot
+echo "
+ ==============================================
+|         Coded By Mr.Robot                    | 
+|         Now Type python3 IP-information.py   |
+ ============================================== "
